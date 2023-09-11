b69edc756878d2d5d0c1f99e9a7f69ef894b94d2
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-09-11 15:15:54 +0530

    Basic calculator in java

---
+++ basics/java/Calculater.java
@@ -0,0 +1,43 @@
+package basics.java;
+
+import java.util.Scanner;
+
+public class Calculater {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		//Take input from user till user does not press X or x
+		while(true) {
+			//Taking operator from user
+			System.out.print("Enter the Operator-");
+		    char op = s.next().trim().charAt(0);
+			if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+				System.out.print("Enter first operands - ");
+				float num1 = s.nextFloat();
+				System.out.print("Enter the second operands - ");
+				float num2 = s.nextFloat();
+				
+				if(op == '+') {
+					float ans = num1 + num2;
+					System.out.println("Sum of the numbers is :: "+ ans);
+				}
+				if(op == '-') {
+					float ans = num1 - num2;
+					System.out.println("Sum of the numbers is :: "+ ans);
+				}
+				if(op == '*') {
+					float sum = num1 * num2;
+					System.out.println("Sum of the numbers is :: "+ sum);
+				}
+				if(op == '/') {
+					float sum = num1 / num2;
+					System.out.println("Sum of the numbers is :: "+ sum);
+				}
+				if(op == '%') {
+					float sum = num1 % num2;
+					System.out.println("Sum of the numbers is :: "+ sum);
+				}
+			}			
+		}
+	}
+}
