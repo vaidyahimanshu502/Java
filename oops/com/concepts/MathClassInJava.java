9dd34d32eff0c84296f441e1ccd3e266ab167d80
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-09-13 07:17:21 +0530

    Implementing Math class of java

---
+++ oops/com/concepts/MathClassInJava.java
@@ -0,0 +1,25 @@
+package oops.com.concepts;
+
+public class MathClassInJava {
+
+	public static void main(String[] args) {
+		double x = 399;
+		double y = 249;
+		
+		// Finding max
+		System.out.println("Maximum between x and y is "+ Math.max(x, y));
+		
+		// Finding min
+		System.out.println("Minimum between x and y is "+ Math.min(x, y));
+		
+		// Finding Square root
+		System.out.println("Square root of x is "+ Math.sqrt(x));
+		
+		//Finding power
+		int a = 5;
+		int b = 3;
+		System.out.println("Power of a to b is "+ Math.pow(a, b));
+
+	}
+
+}
