297ca12fa000242140735218319f1854ecacf016
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-09-13 08:27:09 +0530

    Strings in java

---
+++ strings/StringBasics.java
@@ -0,0 +1,11 @@
+package com.strings;
+
+public class StringBasics {
+
+	public static void main(String[] args) {
+		char[] ch = {'h','i','m','a','n','s','h','u'};
+		String name = new String(ch);
+		System.out.println(name);
+	}
+
+}
