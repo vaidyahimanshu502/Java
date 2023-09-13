0dd3eeabb5751edea3814de511b7af0fd42a907a
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-09-13 07:25:38 +0530

    Some more calculation based on math class

---
+++ oops/com/concepts/SomeMoreMath.java
@@ -0,0 +1,20 @@
+package oops.com.concepts;
+
+public class SomeMoreMath {
+
+	public static void main(String[] args) {
+		double x = 365.999856;
+		
+		//Getting absolute value
+		System.out.println("Absolute value of x is "+ Math.abs(x));
+		
+		// Rounding off
+		System.out.println("Round of value of x is "+ Math.round(x));
+		
+		//Finding cuberoot
+		int a = 125;
+		System.out.println("Cube root of a is "+ Math.cbrt(a));
+
+	}
+
+}
